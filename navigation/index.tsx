d3506b532c5c73897f0b95ecd0ec8708c194dad8
powdevlin12 <thudatdl123@gmail.com>
2023-11-13 23:57:16 +0700

    delete View

---
+++ navigation/index.tsx
@@ -0,0 +1,26 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+import HomeScreen from '../screens/app/HomeScreen';
+import NotificationScreen from '../screens/app/NotificationScreen';
+import ProfileScreen from '../screens/app/ProfileScreen';
+import SettingScreen from '../screens/app/SettingScreen';
+
+type AppStackParamList = {
+  Home: undefined;
+  Notifications: undefined;
+  Profile: undefined;
+  Settings: undefined;
+};
+
+const Stack = createStackNavigator<AppStackParamList>();
+
+function AppStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Notifications" component={NotificationScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingScreen} />
+    </Stack.Navigator>
+  );
+}
