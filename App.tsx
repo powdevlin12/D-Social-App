cb30fc075e65844e5968879324202d1745c21c31
powdevlin12 <thudatdl123@gmail.com>
2023-12-14 23:53:29 +0700

    add redux toolkit

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import 'react-native-gesture-handler';
 import ContainerNavigation from './src/navigation';
+import { Provider } from 'react-redux';
+import store from './src/redux';
 
 export default function App() {
   return (
+    <Provider store={store}>
       <ContainerNavigation />
+    </Provider>
   );
 }
--- src/screens/app/HomeScreen.tsx
+++ src/screens/app/HomeScreen.tsx
@@ -3,11 +3,17 @@ import React from 'react'
 import { BottomTabNavigationProp, BottomTabScreenProps } from '@react-navigation/bottom-tabs'
 import { AppStackParamList } from '../../navigation'
 import { useNavigation } from '@react-navigation/native'
+import { useReduxDispatch, useReduxSelector } from '../../redux'
+import { decrement, increment } from '../../redux/appSlide'
 
 type Props = BottomTabScreenProps<AppStackParamList, 'Home'>
 type HomeScreenNavigationProps = BottomTabNavigationProp<AppStackParamList, 'Home'>
 const HomeScreen = () => {
   const navigation = useNavigation<HomeScreenNavigationProps>()
+
+  const value = useReduxSelector(state => state.app)
+  const dispatch = useReduxDispatch()
+
   return (
     <View style={styles.container}>
       <Button title='Go to Profile' onPress={() => navigation.navigate('Profile', {name : 'Dat'})}/>
@@ -17,6 +23,10 @@ const HomeScreen = () => {
           lang : "en"
         }
       })}/>
+
+      <Text>{value}</Text>
+      <Button title="increment" onPress={() => dispatch(increment(1))} />
+      <Button title="decrement" onPress={() => dispatch(decrement(1))} />
     </View>
   )
 }
