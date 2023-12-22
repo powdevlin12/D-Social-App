0a55b4c24f0feeea3f4a144bd70151b91eeaf48f
powdevlin12 <thudatdl123@gmail.com>
2023-12-22 21:26:28 +0700

    add Loading Screen

--- src/navigation/app/index.tsx
+++ src/navigation/app/index.tsx
@@ -1,25 +1,27 @@
-import { NavigatorScreenParams } from '@react-navigation/native';
-import * as React from 'react';
-import SettingNavigation, { SettingParamList } from './setting/setting.navigation';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../../screens/app/HomeScreen';
-import NotificationScreen from '../../screens/app/NotificationScreen';
-import MessageNavigation from './messages/message.navigation';
+import { NavigatorScreenParams } from "@react-navigation/native";
+import * as React from "react";
+import SettingNavigation, {
+  SettingParamList,
+} from "./setting/setting.navigation";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../../screens/app/HomeScreen";
+import NotificationScreen from "../../screens/app/NotificationScreen";
+import MessageNavigation from "./messages/message.navigation";
+import Loading from "../../screens/common/Loading.screen";
 
 export type AppStackParamList = {
   Home: undefined;
   Notifications: undefined;
-  Message: undefined
+  Message: undefined;
   Settings: NavigatorScreenParams<SettingParamList>;
 };
-
 
 const Stack = createBottomTabNavigator<AppStackParamList>();
 
 const routes: Array<React.ComponentProps<typeof Stack.Screen>> = [
   {
     name: "Home",
-    component: HomeScreen,
+    component: Loading,
   },
   {
     name: "Notifications",
@@ -28,25 +30,25 @@ const routes: Array<React.ComponentProps<typeof Stack.Screen>> = [
   {
     name: "Message",
     component: MessageNavigation,
-    options : {
-      headerShown : false
-    }
+    options: {
+      headerShown: false,
+    },
   },
   {
     name: "Settings",
     component: SettingNavigation,
-    options : {
-      headerShown : false
-    }
+    options: {
+      headerShown: false,
+    },
   },
 ];
 
 export default function NavigationApp() {
   return (
-    <Stack.Navigator initialRouteName='Home'>
-      {
-        routes.map(routeConfig => <Stack.Screen key={routeConfig.name} {...routeConfig} />)
-      }
+    <Stack.Navigator initialRouteName="Home">
+      {routes.map((routeConfig) => (
+        <Stack.Screen key={routeConfig.name} {...routeConfig} />
+      ))}
     </Stack.Navigator>
   );
 }
